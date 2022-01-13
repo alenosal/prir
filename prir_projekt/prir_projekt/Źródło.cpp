dda5822ea2a99dc0083997f18f39649ffa26c437
Miłosz Nosal <milosz.nosal@gmail.com>
2022-01-13 20:24:03 +0100

    basic opm and mpi

--- prir_projekt/prir_projekt/Źródło.cpp
+++ prir_projekt/prir_projekt/Źródło.cpp
@@ -2,7 +2,9 @@
 #include <omp.h>
 #include <stdlib.h>
 #include <time.h>
+#include "mpi.h"
 
+//iloœæ liczb w tabeli
 int N = 10000;
 
 using namespace std;
@@ -18,7 +20,7 @@ int* createTable() {
 }
 
 int basicAverage(int min, int max, int setValue, int* table) {
-	
+
 	int sum = 0, count = 0, average;
 	int num, m, tmp = 0;
 
@@ -75,9 +77,54 @@ int ompAverage(int min, int max, int setValue, int* table) {
 	return average;
 }
 
-int main() {
+int mpiAverage(int min, int max, int setValue, int* table, int argc, char* argv[]) {
+	printf("mpiAverage\n");
+
+	int sum = 0, count = 0, average;
+	int num, m, tmp = 0;
+	int rank, size, length;
+	char name[80];
+
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	clock_t start = clock();
+
+	for (int i = 0; i < N; i++) {
+		if (i % size != rank) continue;
+
+		printf("rank %i working on element %i\n", rank, i);
+
+		num = table[i];
+		while (num > 0) {
+			m = num % 10;
+			tmp = tmp + m;
+			num = num / 10;
+		}
+		if (min < table[i] && table[i]<max && tmp > setValue) {
+			sum = sum + table[i];
+			count++;
+		}
+		tmp = 0;
+	}
+
+	average = sum / count;
+	clock_t end = clock();
+	float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+	printf("%f\n", seconds);
+
+	MPI_Finalize();
+
+	return average;
+}
+
+int main(int argc, char* argv[]) {
 	int* table = createTable();
 	printf("%d\n", basicAverage(10000, 34000, 6, table));;
 	printf("%d\n", ompAverage(10000, 34000, 6, table));
+	printf("%d\n", mpiAverage(10000, 34000, 6, table, argc, argv));
+
 	return 0;
 }
