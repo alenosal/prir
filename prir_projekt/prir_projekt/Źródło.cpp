5f3b63fe02e6c5a8769312c265f94db6e0ce24f1
adammachaj <37911187+adammachaj@users.noreply.github.com>
2022-01-13 21:28:58 +0100

    mpi

--- prir_projekt/prir_projekt/Źródło.cpp
+++ prir_projekt/prir_projekt/Źródło.cpp
@@ -10,121 +10,141 @@ int N = 10000;
 using namespace std;
 
 int* createTable() {
-	int* table = (int*)malloc(sizeof(int) * N);
+  int* table = (int*)malloc(sizeof(int) * N);
 
-
-	for (int i = 0; i < N; i++) {
-		table[i] = rand() % (100000 - 10000) + 10000;
-	}
-	return table;
+  int i = 0;
+  for (i; i < N; i++) {
+    table[i] = rand() % (100000 - 10000) + 10000;
+  }
+  return table;
 }
 
 int basicAverage(int min, int max, int setValue, int* table) {
 
-	int sum = 0, count = 0, average;
-	int num, m, tmp = 0;
+  int sum = 0, count = 0, average;
+  int num, m, tmp = 0;
 
-	clock_t start = clock();
+  clock_t start = clock();
 
-	for (int i = 0; i < N; i++) {
-		num = table[i];
-		while (num > 0) {
-			m = num % 10;
-			tmp = tmp + m;
-			num = num / 10;
-		}
-		if (min < table[i] && table[i]<max && tmp > setValue) {
-			sum = sum + table[i];
-			count++;
-		}
-		tmp = 0;
-	}
+  int i = 0;
+  for (i; i < N; i++) {
+    num = table[i];
+    while (num > 0) {
+      m = num % 10;
+      tmp = tmp + m;
+      num = num / 10;
+    }
+    if (min < table[i] && table[i]<max && tmp > setValue) {
+      sum = sum + table[i];
+      count++;
+    }
+    tmp = 0;
+  }
 
-	average = sum / count;
-	clock_t end = clock();
-	float seconds = (float)(end - start) / CLOCKS_PER_SEC;
-	printf("%f\n", seconds);
+  average = sum / count;
+  clock_t end = clock();
+  float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+  printf("%f\n", seconds);
 
-	return average;
+  return average;
 }
-
 int ompAverage(int min, int max, int setValue, int* table) {
-	int sum = 0, count = 0, average;
-	int num, m, tmp = 0;
+  int sum = 0, count = 0, average;
+  int num, m, tmp = 0;
 
-	clock_t start = clock();
+  clock_t start = clock();
 
 #pragma omp parallel for num_threads(9)
-	for (int i = 0; i < N; i++) {
-		num = table[i];
-		while (num > 0) {
-			m = num % 10;
-			tmp = tmp + m;
-			num = num / 10;
-		}
-		if (min < table[i] && table[i]<max && tmp > setValue) {
-			sum = sum + table[i];
-			count++;
-		}
-		tmp = 0;
-	}
 
-	average = sum / count;
-	clock_t end = clock();
-	float seconds = (float)(end - start) / CLOCKS_PER_SEC;
-	printf("%f\n", seconds);
+  int i = 0;
+  for (i = 0; i < N; i++) {
+    num = table[i];
+    while (num > 0) {
+      m = num % 10;
+      tmp = tmp + m;
+      num = num / 10;
+    }
+    if (min < table[i] && table[i]<max && tmp > setValue) {
+      sum = sum + table[i];
+      count++;
+    }
+    tmp = 0;
+  }
 
-	return average;
+  average = sum / count;
+  clock_t end = clock();
+  float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+  printf("%f\n", seconds);
+
+  return average;
 }
 
 int mpiAverage(int min, int max, int setValue, int* table, int argc, char* argv[]) {
-	printf("mpiAverage\n");
+  printf("mpiAverage\n");
 
-	int sum = 0, count = 0, average;
-	int num, m, tmp = 0;
-	int rank, size, length;
-	char name[80];
+  int sum = 0, count = 0, average;
+  int num, m, tmp = 0, number = 0;
+  int rank, size, length;
+  char name[80];
 
-	MPI_Init(&argc, &argv);
+  MPI_Status status;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
+  clock_t start = MPI_Wtime();
+  int i = 0;
+  if (rank == 0)
+  {
+    start = MPI_Wtime();
 
-	clock_t start = clock();
+    printf("rank = 0\n");
 
-	for (int i = 0; i < N; i++) {
-		if (i % size != rank) continue;
+    for (i = 1; i < size; i++)
+      MPI_Send(table, sizeof(table) / sizeof(int), MPI_INT, i, 0, MPI_COMM_WORLD);
+  }
 
-		printf("rank %i working on element %i\n", rank, i);
+  else
+  {
+    printf("else\n");
 
-		num = table[i];
-		while (num > 0) {
-			m = num % 10;
-			tmp = tmp + m;
-			num = num / 10;
-		}
-		if (min < table[i] && table[i]<max && tmp > setValue) {
-			sum = sum + table[i];
-			count++;
-		}
-		tmp = 0;
-	}
+    MPI_Get_count(&status, MPI_INT, &number);
+    MPI_Recv(table, sizeof(table) / sizeof(int), MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 
-	average = sum / count;
-	clock_t end = clock();
-	float seconds = (float)(end - start) / CLOCKS_PER_SEC;
-	printf("%f\n", seconds);
+    printf("rank: %d\n", rank);
 
-	MPI_Finalize();
+    for (i; i < N; i++) {
+      num = table[i];
+      while (num > 0) {
+        m = num % 10;
+        tmp = tmp + m;
+        num = num / 10;
+      }
+      if (min < table[i] && table[i]<max && tmp > setValue) {
+        sum = sum + table[i];
+        count++;
+      }
+      tmp = 0;
+    }
 
-	return average;
+    printf("suma: %d\n", sum);
+  }
+
+  average = sum / count;
+
+  float seconds = (float)(MPI_Wtime() - start) / CLOCKS_PER_SEC;
+  printf("%f\n", seconds);
+
+  MPI_Finalize();
+
+  return average;
 }
 
 int main(int argc, char* argv[]) {
-	int* table = createTable();
-	printf("%d\n", basicAverage(10000, 34000, 6, table));;
-	printf("%d\n", ompAverage(10000, 34000, 6, table));
-	printf("%d\n", mpiAverage(10000, 34000, 6, table, argc, argv));
+  int* table = createTable();
+  printf("%d\n", basicAverage(10000, 34000, 6, table));;
+  printf("%d\n", ompAverage(10000, 34000, 6, table));
+  printf("%d\n", mpiAverage(10000, 34000, 6, table, argc, argv));
 
-	return 0;
+  return 0;
 }
